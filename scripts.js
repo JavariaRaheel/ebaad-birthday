3d0ad864e399ded778c3b0107e8e8bb894b81c8b
bintemukhtar1803@gmail.com <bintemukhtar1803@gmail.com>
2024-08-24 19:41:20 +0500

    not a

---
+++ scripts.js
@@ -0,0 +1,7 @@
+document.getElementById('wishSakina').addEventListener('click', function() {
+    alert("Happy Birthday, Ebaad! Wishing you a day filled with happiness and a year filled with joy. Sakina.");
+});
+
+document.getElementById('wishHussain').addEventListener('click', function() {
+    alert("Happy Birthday, Ebaad! May all your wishes come true and your day be as special as you are. Best wishes, Hussain.");
+});
